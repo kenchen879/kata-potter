301acef33c36f171dc29f0dbeb0af899aa440a66
kenchen879 <57173487+kenchen879@users.noreply.github.com>
2022-05-16 16:36:22 +0800

    第一個紅燈

--- apps/potter/src/app/potter.spec.ts
+++ apps/potter/src/app/potter.spec.ts
@@ -1,7 +1,23 @@
 import { Potter } from './potter';
 
-describe('Potter', () => {
+describe('Kata - Harry Potter Book', () => {
   it('should create an instance', () => {
     expect(new Potter()).toBeTruthy();
+  });
+
+  test('testBasics: buy one book', () => {
+    const potter = new Potter();
+    potter.addToBasket([]);
+    expect(potter.price).toBe(0);
+    potter.addToBasket([1]);
+    expect(potter.price).toBe(8);
+    potter.addToBasket([2]);
+    expect(potter.price).toBe(8);
+    potter.addToBasket([3]);
+    expect(potter.price).toBe(8);
+    potter.addToBasket([4]);
+    expect(potter.price).toBe(8);
+    potter.addToBasket([1, 1, 1]);
+    expect(potter.price).toBe(8 * 3);
   });
 });
--- apps/potter/src/app/potter.ts
+++ apps/potter/src/app/potter.ts
@@ -1,2 +1,6 @@
 export class Potter {
+    addToBasket(book: number[]) {}
+    get price() {
+        return -1;
+    }
 }
