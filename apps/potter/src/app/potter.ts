46e7501cc9384efedd8009ff705cf7104fde1a2c
kenchen879 <57173487+kenchen879@users.noreply.github.com>
2022-05-16 18:18:08 +0800

    第三個綠燈

--- apps/potter/src/app/potter.ts
+++ apps/potter/src/app/potter.ts
@@ -6,37 +6,43 @@ export class Potter {
             this._basket.push(book[i]);
     }
     get price() {
-        let price = 8;
-        let discount = [1, 1, 0.95, 0.9, 0.8, 0.75];
+        let price = 0;
+        const discount = [1, 1, 0.95, 0.9, 0.8, 0.75];
         let totalBookNumber = 0;
+        // 取得不重複的購物籃
+        let distinctBasket = [];
         let distinctBookNumber = 0;
+        let index = [];
 
         totalBookNumber = this._basket.length;
 
-        // 取得不重複的購物籃
-        const distinctBasket = this.distinctBasket;
-        // 取得不重複購物籃的數量
-        distinctBookNumber = distinctBasket.length;
-
-        if (distinctBookNumber == 1) {
-            price *= totalBookNumber;
-        } else {
-            switch (totalBookNumber) {
+        while (totalBookNumber > 0) {
+            // 取得不重複的購物籃
+            distinctBasket = this.distinctBasket;
+            // 取得不重複購物籃的數量
+            distinctBookNumber = distinctBasket.length;
+            switch (distinctBookNumber) {
+                case 1:
+                    price += 8 * distinctBookNumber;
+                    break;
                 case 2:
-                    price *= 2 * discount[2];
+                    price += 8 * 2 * discount[2];
                     break;
                 case 3:
-                    price *= 3 * discount[3];
+                    price += 8 * 3 * discount[3];
                     break;
                 case 4:
-                    price *= 4 * discount[4];
+                    price += 8 * 4 * discount[4];
                     break;
                 case 5:
-                    price *= 5 * discount[5];
+                    price += 8 * 5 * discount[5];
                     break;
                 default:
                     price = 0;
             }
+            index = distinctBasket.filter(e => this._basket.indexOf(e));
+            index.forEach(e => this._basket.splice(e, distinctBookNumber));
+            totalBookNumber -= distinctBookNumber;
         }
         return price;
     }
