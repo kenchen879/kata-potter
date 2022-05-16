5428adb2de5941183c0712e9cb83542be0534d74
kenchen879 <57173487+kenchen879@users.noreply.github.com>
2022-05-16 17:03:27 +0800

    第二個綠燈

--- apps/potter/src/app/potter.ts
+++ apps/potter/src/app/potter.ts
@@ -7,10 +7,35 @@ export class Potter {
     }
     get price() {
         let price = 0;
-        let totalBooks = 0;
-        totalBooks = this._basket.length;
-        console.log(totalBooks);
-        price = 8 * totalBooks;
+        let totalBookNumber = 0;
+        let distunctBookNumber = 0;
+
+        totalBookNumber = this._basket.length;
+        const distinctBasket = this._basket.filter((ele , pos) => {
+            return this._basket.indexOf(ele) == pos;
+        });
+        distunctBookNumber = distinctBasket.length;
+
+        if (distunctBookNumber == 1) {
+            price = 8 * totalBookNumber;
+        } else {
+            switch (totalBookNumber) {
+                case 2:
+                    price = 8 * 2 * 0.95;
+                    break;
+                case 3:
+                    price = 8 * 3 * 0.9;
+                    break;
+                case 4:
+                    price = 8 * 4 * 0.8;
+                    break;
+                case 5:
+                    price = 8 * 5 * 0.75;
+                    break;
+                default:
+                    break;
+            }
+        }
         return price;
     }
 }
