0ab39dd4b5056ab0c81ff2506f1b9592788d52ff
kenchen879 <57173487+kenchen879@users.noreply.github.com>
2022-05-16 16:47:04 +0800

    第一次重構

--- apps/potter/src/app/potter.spec.ts
+++ apps/potter/src/app/potter.spec.ts
@@ -1,27 +1,26 @@
 import { Potter } from './potter';
 
 describe('Kata - Harry Potter Book', () => {
+  let potter: Potter;
+
+  beforeEach(() => {
+    potter = new Potter();
+  });
   it('should create an instance', () => {
-    expect(new Potter()).toBeTruthy();
+    expect(potter).toBeTruthy();
   });
 
   test('testBasics: buy one book', () => {
-    let potter = new Potter();
     potter.addToBasket([]);
     expect(potter.price).toBe(0);
-    potter = new Potter();
     potter.addToBasket([1]);
     expect(potter.price).toBe(8);
-    potter = new Potter();
     potter.addToBasket([2]);
     expect(potter.price).toBe(8);
-    potter = new Potter();
     potter.addToBasket([3]);
     expect(potter.price).toBe(8);
-    potter = new Potter();
     potter.addToBasket([4]);
     expect(potter.price).toBe(8);
-    potter = new Potter();
     potter.addToBasket([1, 1, 1]);
     expect(potter.price).toBe(8 * 3);
   });
--- apps/potter/src/app/potter.ts
+++ apps/potter/src/app/potter.ts
@@ -1,6 +1,7 @@
 export class Potter {
     private _basket: number[] = [];
     addToBasket(book: number[]) {
+        this._basket = [];
         for (let i = 0; i < book.length; i++)
             this._basket.push(book[i]);
     }
