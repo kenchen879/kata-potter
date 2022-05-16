616edd244d78c260a14cec9b1aa4231c999b013b
kenchen879 <57173487+kenchen879@users.noreply.github.com>
2022-05-16 18:46:09 +0800

    第四個紅燈

--- apps/potter/src/app/potter.spec.ts
+++ apps/potter/src/app/potter.spec.ts
@@ -45,4 +45,15 @@ describe('Kata - Harry Potter Book', () => {
     potter.addToBasket([0, 1, 1, 2, 3, 4]);
     expect(potter.price).toBe(8 + (8 * 5 * 0.75));
   });
+
+  test('testEdgeCases', () => {
+    potter.addToBasket([0, 0, 1, 1, 2, 2, 3, 4]);
+    expect(potter.price).toBe(2 * (8 * 4 * 0.8));
+    potter.addToBasket([0, 0, 0, 0, 0, 
+                        1, 1, 1, 1, 1, 
+                        2, 2, 2, 2, 
+                        3, 3, 3, 3, 3, 
+                        4, 4, 4, 4]);
+    expect(potter.price).toBe(3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8));
+  });
 });
